4231657e66e73649f033e38d7cb9673d20076799
Teodora Tsvetanov <teodora.tsvetanov@gmail.com>
2021-06-29 19:58:30 -0500

    polymorphism practice

---
+++ src/day58_polymorphism/Father.java
@@ -0,0 +1,15 @@
+package day58_polymorphism;
+
+public class Father {
+    public void raiseKid(){
+        System.out.println("Father is raising the kid");
+    }
+
+    public void feedKid(){
+        System.out.println("Father is feeding the kid");
+    }
+
+    public void playWithKid(){
+        System.out.println("Father is playing with his kid");
+    }
+}
